b4482feeb1b3248f29c4fe50f9e18c2408ffae1c
Phillip Oertel <me@phillipoertel.com>
2009-04-14 01:46:35 +0200

    another thing that wouldnt happen if you had tests ...

--- all_tweets_must_die.rb
+++ all_tweets_must_die.rb
@@ -7,7 +7,7 @@ require 'login'
 # TODO write Tweet class and move this and the delete there.
 def tweet_should_live?(tweet)
   hours_to_live = tweet['text'].match(/#keep(\d+)h$/)[1].to_i rescue 1
-  has_lived = (Time.now.utc - Time.parse(tweet['text']))
+  has_lived = (Time.now.utc - Time.parse(tweet['created_at']))
   should_live = (hours_to_live * 60 * 60)
   has_lived < should_live
 end
