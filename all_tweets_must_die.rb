cc423a2e8b78ac62b80f5aca7f5fdd97dcabfb5b
Phillip Oertel <me@phillipoertel.com>
2009-06-12 16:30:42 +0200

    mini improvements.

--- all_tweets_must_die.rb
+++ all_tweets_must_die.rb
@@ -4,12 +4,14 @@ require 'time'
 require 'lib/rtwitter'
 require 'login'
 
+DEFAULT_TWEET_AGE_IN_HOURS = 12
+
 # TODO write Tweet class and move this and the delete there.
 def tweet_should_live?(tweet)
-  hours_to_live = tweet['text'].match(/#keep(\d+)h$/)[1].to_i rescue 1
-  has_lived = (Time.now.utc - Time.parse(tweet['created_at']))
-  should_live = (hours_to_live * 60 * 60)
-  has_lived < should_live
+  hours_to_live = tweet['text'].match(/#keep(\d+)h$/)[1].to_i rescue DEFAULT_TWEET_AGE_IN_HOURS
+  current_age = (Time.now.utc - Time.parse(tweet['created_at']))
+  maximum_age = (hours_to_live * 60 * 60)
+  current_age <= maximum_age
 end
 
 my_tweets = RTwitter::Status.new(:user => @login[:user], :password => @login[:password]).user_timeline
